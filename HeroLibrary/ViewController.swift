9c5ded4d73481d26756c1aa63a97572f9f105551
FOliveira <filipe.oliveira@syonet.com>
2020-09-12 20:45:30 -0300

    Initial Commit

---
+++ HeroLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeroLibrary
+//
+//  Created by Foliveira on 12/09/20.
+//  Copyright © 2020 filipero.dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
