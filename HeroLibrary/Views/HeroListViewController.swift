f3938917419cf18d64a1e96a2809a5212008f8c9
FOliveira <filipe.oliveira@syonet.com>
2020-09-12 21:03:51 -0300

    Criados arquivos base para o app

--- HeroLibrary/Views/HeroListViewController.swift
+++ HeroLibrary/Views/HeroListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HeroListViewController.swift
 //  HeroLibrary
 //
 //  Created by Foliveira on 12/09/20.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HeroListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
